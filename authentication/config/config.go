494fa3725614f21d3d4b3a13a42096efc5735805
Water.Li <lizongbin@hotmail.com>
2022-05-08 07:50:17 +0800

    Update config.go

--- authentication/config/config.go
+++ authentication/config/config.go
@@ -2,7 +2,7 @@ package config
 
 const DRIVER_NAME string = "mysql"
 
-const MYSQL_CONFIG string = "root:RxDragDb@tcp(localhost:3306)/entity_engine?parseTime=true"
+const MYSQL_CONFIG string = "root:RxDragDb@tcp(localhost:3306)/entify-schemas?parseTime=true"
 
 const SERVICE_ID uint64 = 1
 

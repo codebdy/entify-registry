927bf8a8d41aaad269ba8a8e8ea364cefd54645e
Water.Li <lizongbin@hotmail.com>
2022-05-10 20:15:54 +0800

    env config can work

--- config/config.go
+++ config/config.go
@@ -1,25 +1,48 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"rxdrag.com/entify-schema-registry/consts"
+)
 
-var theConfig config
+var c config
 
 type config struct {
 	v *viper.Viper
 }
 
-func Init() {
+const (
+	TRUE  = "true"
+	FALSE = "false"
+)
 
+func Init() {
+	c.v = viper.New()
+	c.v.SetEnvPrefix(consts.CONFIG_PREFIX)
+	c.v.BindEnv(consts.DB_USER)
+	c.v.BindEnv(consts.DB_PASSWORD)
+	c.v.BindEnv(consts.DB_HOST)
+	c.v.BindEnv(consts.DB_PORT)
+	c.v.BindEnv(consts.DB_SCHEMA)
+	c.v.BindEnv(consts.IS_INSTALLED)
 }
 
 func GetString(key string) string {
-	return ""
+	return c.v.Get(key).(string)
 }
 
-func GetBoolean(key string) bool {
-	return false
+func GetBool(key string) bool {
+	return c.v.Get(key) == TRUE
 }
 
-func SetValue(key string, value interface{}) {
+func SetString(key string, value string) {
+	c.v.BindEnv(key, value)
+}
 
+func SetBool(key string, value bool) {
+	if value {
+		c.v.BindEnv(key, TRUE)
+	} else {
+		c.v.BindEnv(key, FALSE)
+	}
 }

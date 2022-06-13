ad27244600783a7dadfd7400bb41efbe953e6502
Water.Li <lizongbin@hotmail.com>
2022-06-13 11:53:13 +0800

    Update index.js

--- gateway-example/index.js
+++ gateway-example/index.js
@@ -1,14 +1,19 @@
 const { ApolloServer, gql } = require("apollo-server");
-const { ApolloGateway } = require("@apollo/gateway");
+const { ApolloGateway, IntrospectAndCompose } = require("@apollo/gateway");
 const { readFileSync } = require("fs");
+
+const port = 8081;
 
 // Initialize an ApolloGateway instance and pass it
 // the supergraph schema as a string
 const gateway = new ApolloGateway({
-  serviceList: [
-    { name: "people", url: "http://localhost:4001" },
-    { name: "films", url: "http://localhost:4002" },
-  ],
+  supergraphSdl: new IntrospectAndCompose({
+    subgraphs: [
+      { name: "accounts", url: "http://localhost:4000/graphql" },
+      // { name: "products", url: "http://localhost:4002" },
+      // ...additional subgraphs...
+    ],
+  }),
 });
 
 // Pass the ApolloGateway to the ApolloServer constructor
@@ -16,6 +21,6 @@ const server = new ApolloServer({
   gateway,
 });
 
-server.listen({ port: 8081 }).then(({ url }) => {
+server.listen({ port }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });

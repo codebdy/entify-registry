ab9947f92234932373dd712c988d8d71d80def03
Water.Li <lizongbin@hotmail.com>
2022-06-13 11:17:21 +0800

    Create index.js

---
+++ gateway-example/index.js
@@ -0,0 +1,20 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { ApolloGateway } = require('@apollo/gateway');
+const { readFileSync } = require('fs');
+
+const supergraphSdl = readFileSync('./supergraph.graphql').toString();
+
+// Initialize an ApolloGateway instance and pass it
+// the supergraph schema as a string
+const gateway = new ApolloGateway({
+  supergraphSdl,
+});
+
+// Pass the ApolloGateway to the ApolloServer constructor
+const server = new ApolloServer({
+  gateway,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});

e5b81c94c5483d6c6c8deb81550dcf2569904e03
Water.Li <lizongbin@hotmail.com>
2022-06-17 12:57:32 +0800

    add graphql-request

--- gateway-example/index.js
+++ gateway-example/index.js
@@ -1,9 +1,35 @@
 const { ApolloServer, gql } = require("apollo-server");
 const { ApolloGateway, IntrospectAndCompose } = require("@apollo/gateway");
 const { readFileSync } = require("fs");
+import { GraphQLClient } from "graphql-request";
 
 const port = 8081;
+const gqlstr = `
+  query{
+    services{
+      id
+      name
+      url
+    }
+  }
+`;
 
+const graphQLClient = new GraphQLClient(
+  "https://localhost:8080/grahpql",
+  {
+    mode: "cors",
+  }
+);
+graphQLClient
+  .request(gqlstr)
+  .then((data) => {
+    if (data) {
+      console.log(data);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 // Initialize an ApolloGateway instance and pass it
 // the supergraph schema as a string
 const gateway = new ApolloGateway({
